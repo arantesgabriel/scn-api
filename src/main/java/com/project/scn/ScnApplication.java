c92fc3ef839741c9e40d9e1a2084dfebe47c7302
Gabriel Arantes <gabrielpacontato@gmail.com>
2023-10-27 20:34:51 -0300

    Início do projeto.

---
+++ src/main/java/com/project/scn/ScnApplication.java
@@ -0,0 +1,13 @@
+package com.project.scn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/scn/ScnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.scn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
