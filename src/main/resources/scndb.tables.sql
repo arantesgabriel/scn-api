d8e708e03fc565f0c0b113b8603cd4907e7e8265
Gabriel Arantes <gabrielpacontato@gmail.com>
2023-11-12 02:20:46 -0300

    Ajustado tabelas e objetos para implementar um novo fluxo de controle de notas.

--- src/main/resources/scndb.tables.sql
+++ src/main/resources/scndb.tables.sql
@@ -1,64 +1,83 @@
+CREATE TABLE scndb.tcurso
+(
+    codcs  INT     NOT NULL AUTO_INCREMENT,
+    nomecs VARCHAR(255),
+    durccs NUMERIC NOT NULL,
+    codaln INT,
+    PRIMARY KEY (codcs),
+    FOREIGN KEY (codaln)
+        REFERENCES scndb.taluno (codaln)
+);
+
 CREATE TABLE scndb.taluno
 (
     codaln   INT NOT NULL AUTO_INCREMENT,
-	nomealn  VARCHAR(255),
-	usraln   VARCHAR(255),
+    nomealn  VARCHAR(255),
+    usraln   VARCHAR(255),
     senhaaln VARCHAR(255),
-	codcs    INT NOT NULL,
+    codsm    INT,
+    coddcp   INT,
     PRIMARY KEY (codaln),
-    FOREIGN KEY (codcs) REFERENCES scndb.tcurso (codcs)
-);
-
-CREATE TABLE scndb.tcurso
-(
-    codcs     INT NOT NULL AUTO_INCREMENT,
-	nomecs    VARCHAR(255),
-	duracaocs NUMERIC NOT NULL,
-    codsm     INT,
-    PRIMARY KEY (codcs),
-    FOREIGN KEY (codsm) REFERENCES scndb.tsemestre (codsm)
+    FOREIGN KEY (codsm)
+        REFERENCES scndb.tsemestre (codsm),
+    FOREIGN KEY (coddcp)
+        REFERENCES scndb.tdisciplina (coddcp)
 );
 
 CREATE TABLE scndb.tsemestre
 (
-    codsm        INT AUTO_INCREMENT,
-	nomedsm      VARCHAR(255),
-	mediasm      DECIMAL,
-    statusaprvsm BOOLEAN,
-    coddcp       INT NOT NULL,
-    PRIMARY KEY (codsm),
-    FOREIGN KEY (coddcp) REFERENCES scndb.tdisciplina (coddcp)
+    codsm   INT AUTO_INCREMENT,
+    numpr   NUMERIC,
+    mediasm DECIMAL DEFAULT 0,
+    PRIMARY KEY (codsm)
 );
 
 CREATE TABLE scndb.tdisciplina
 (
     coddcp       INT AUTO_INCREMENT,
-	nomedcp      VARCHAR(255),
-	qtdfaltadcp  INTEGER,
-	professordcp VARCHAR(150),
-	totnotadcp   DECIMAL DEFAULT 0,
+    nomedcp      VARCHAR(255),
+    qtdfaltadcp  INTEGER,
+    professordcp VARCHAR(150),
+    totnotadcp   DECIMAL DEFAULT 0,
+    statapr      BOOLEAN DEFAULT FALSE,
     codavl       INT,
     codtrb       INT,
     PRIMARY KEY (coddcp),
-    FOREIGN KEY (codtrb) REFERENCES scndb.ttrabalho (codtrb),
-    FOREIGN KEY (codavl) REFERENCES scndb.tavaliacao (codavl)
+    FOREIGN KEY (codtrb)
+        REFERENCES scndb.ttrabalho (codtrb),
+    FOREIGN KEY (codavl)
+        REFERENCES scndb.tavaliacao (codavl)
+);
+
+CREATE TABLE scndb.tgrade
+(
+    codgrd INT AUTO_INCREMENT,
+    numpr  VARCHAR(255),
+    codcs  INT,
+    coddcp INT,
+    PRIMARY KEY (codgrd),
+    FOREIGN KEY (codcs)
+        REFERENCES scndb.tdisciplina (codcs),
+    FOREIGN KEY (coddcp)
+        REFERENCES scndb.tdisciplina (coddcp)
 );
 
 CREATE TABLE scndb.ttrabalho
 (
-    codtrb  INT AUTO_INCREMENT,
-    nometrb VARCHAR(255),
+    codtrb     INT AUTO_INCREMENT,
+    nometrb    VARCHAR(255),
     notatottrb DECIMAL NOT NULL,
     notafintrb DECIMAL NOT NULL,
-    datent  DATE,
+    datrb      DATE,
     PRIMARY KEY (codtrb)
 );
 
-CREATE TABLE scndb.tavaliacao (
-    codavl INT AUTO_INCREMENT,
-    nomeavl VARCHAR(255),
+CREATE TABLE scndb.tavaliacao
+(
+    codavl     INT AUTO_INCREMENT,
+    nomeavl    VARCHAR(255),
     notatotavl DECIMAL NOT NULL,
     notafinavl DECIMAL NOT NULL,
-	datent  DATE,
+    datavl     DATE,
     PRIMARY KEY (codavl)
 );
